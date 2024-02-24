3214b5909ba6405c41ef9e9860ad52e4560ab51c
Mahendran Ponnusamy <mahendran.ponnusamy@nordstrom.com>
2024-02-24 11:32:22 -0800

    Testing listing buckets. and checkgin TF vars file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -16,3 +16,17 @@ module "cloud_storage" {
   object_path = var.object_path
   user = var.user
 }
+
+module "cloud_instance" {
+  source = "../../modules/cloud_instance"
+  project = var.project
+  env     = local.env
+  app_id   = var.app_id
+  user = var.user
+  cloud_instance_name = var.cloud_instance_name
+  database_name = var.database_name
+  sql_user = var.sql_user
+  sql_user_password =var.sql_user_password
+  database_version = var.database_version
+  local_ip_address = var.local_ip_address
+}
